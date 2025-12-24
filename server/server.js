b521b06ca6e88b7eced4a8829f710145e7d8785a
imsathishjk <sathishjk01@gmail.com>
2025-12-24 14:23:39 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -24,6 +24,14 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://rich-blog-app-client.vercel.app'); // Or your client's origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/api', authRoute);
 app.use('/api', blogRoute)
 
